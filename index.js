30b99fc02f435742941bf09e4be2d4c1452b0d69
Yoshua Wuyts <i@yoshuawuyts.com>
2015-10-11 19:22:51 +1100

    script: install deps

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const readPackage = require('read-closest-package')
+const install = require('npm-install-package')
 const copy = require('copy-template-dir')
 const mapLimit = require('map-limit')
 const eos = require('end-of-stream')
@@ -12,7 +13,7 @@ module.exports = initializeCli
 // (obj, fn) -> null
 function initializeCli (argv, cb) {
   argv.dependencies = [ 'cliclopts', 'minimist', 'pump' ]
-  const tasks = [ mkdir, parsePkg, copyFiles, setMod ]
+  const tasks = [ mkdir, parsePkg, copyFiles, setMod, installDeps ]
 
   mapLimit(tasks, 1, iterator, cb)
   function iterator (fn, next) {
@@ -63,4 +64,15 @@ function copyFiles (argv, cb) {
 function setMod (argv, next) {
   const route = path.join(process.cwd(), 'bin/cli.js')
   fs.chmod(route, '755', next)
+}
+
+// install dependencies from npm for app-main
+// (obj, cb) -> null
+function installDeps (argv, next) {
+  const opts = { save: true, cache: true }
+  install(argv.dependencies, opts, function (err) {
+    if (err) return next(err)
+    process.chdir(path.join(process.cwd(), '..'))
+    next()
+  })
 }
